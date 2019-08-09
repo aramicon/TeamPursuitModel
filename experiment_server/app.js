f4857a291399c9a98d6c20fe53659efcd90f3792
DONAL PATRICK KELLY <01364189@it.nuigalway.ie>
2019-08-09 18:40:13 +0100

    finished todo app now need to convert it

--- experiment_server/app.js
+++ experiment_server/app.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
+const Joi = require('joi');
+const db = require('./db');
 const app = express();
+const collection = "todo";
+
+const schema = Joi.object().keys({
+	todo:Joi.string().required()
+});
 
 app.use(bodyParser.json());
 
-const path = require('path');
 
-const db = require('./db');
-const collection = "todo";
+
+
 
 app.get('/',(req,res)=> {
 	res.sendFile(path.join(__dirname,'index.html'));
@@ -17,7 +24,7 @@ app.get('/getTodos',(req,res)=>{
 	db.getDB().collection(collection).find({}).toArray((err,documents)=>{
 		if(err){
 			console.log("error getting collection err " + err);
-			
+
 		}
 		else{
 			console.log(documents);
@@ -31,28 +38,41 @@ app.put('/:id',(req,res)=>{
 	const userInput = req.body;
 	console.log("update existing todo using id");
 	console.log("id " + todoID + " body " + userInput.todo);
-	
+
 	db.getDB().collection(collection).findOneAndUpdate({_id : db.getPrimaryKey(todoID)},{$set : {todo : userInput.todo}},{returnOriginal : false},(err,result)=>{
 		if(err){
 			console.log("error when updating err = " + err);
 		}
 		else{
-			res.json(result);			
+			res.json(result);
 		}
-		
+
 	});
 });
 
-app.post("/",(req,res) => {
+app.post("/",(req,res,next) => {
 	const userInput = req.body;
-	db.getDB().collection(collection).insertOne(userInput,(err,result)=>{
+
+	Joi.validate(userInput, schema, (err,result) =>{
 		if(err){
-			console.log("problem saving new record err "+ err);
+			const error = new Error("Invalid Input adding todo");
+			error.status = 400;
+			next(error);
 		}
 		else{
-			res.json({result:result.result, document: result.ops[0]});
+			db.getDB().collection(collection).insertOne(userInput,(err,result)=>{
+				if(err){
+					const error = new Error("Failed to insert todo");
+					error.status = 400;
+					next(error);
+				}
+				else{
+					res.json({result:result.result, document: result.ops[0],msg:"Successfully inserted todo",err:null});
+				}
+			});
 		}
-	});
+	})
+
 });
 
 app.delete("/:id",(req,res)=>{
@@ -64,6 +84,14 @@ app.delete("/:id",(req,res)=>{
 		}
 		else{
 			res.json(result);
+		}
+	});
+});
+
+app.use((err,req,res,next) => {
+	res.status(err.status).json({
+		error: {
+			message : err.message
 		}
 	});
 });
