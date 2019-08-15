dcb945bfeaea649078ea8ad321d6cf4e08663630
DONAL PATRICK KELLY <01364189@it.nuigalway.ie>
2019-08-15 18:43:05 +0100

    now saves three fields but have to get it to save json

--- experiment_server/app.js
+++ experiment_server/app.js
@@ -4,21 +4,25 @@ const path = require('path');
 const Joi = require('joi');
 const db = require('./db');
 const app = express();
-const collection = "todo";
+const collection = "experiment_settings";
 
 const schema = Joi.object().keys({
-	todo:Joi.string().required(),
-	details:Joi.string()
+	name:Joi.string().required(),
+	global_settings:Joi.object().required(),
+	race_settings:Joi.object().required(),
+	rider_settings:Joi.object().required()
 });
 
 app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/public'));
 
 
 app.get('/',(req,res)=> {
 	res.sendFile(path.join(__dirname,'index.html'));
 });
 
-app.get('/getTodos',(req,res)=>{
+app.get('/getExperimentSettings',(req,res)=>{
 	db.getDB().collection(collection).find({}).toArray((err,documents)=>{
 		if(err){
 			console.log("error getting collection err " + err);
@@ -33,38 +37,37 @@ app.get('/getTodos',(req,res)=>{
 app.put('/:id',(req,res)=>{
 	const todoID = req.params.id;
 	const userInput = req.body;
-	console.log("update existing todo using id");
+	console.log("update existing experiment using id");
 	console.log("id " + todoID + " body " + JSON.stringify(userInput));
 
-	db.getDB().collection(collection).findOneAndUpdate({_id : db.getPrimaryKey(todoID)},{$set : {todo : userInput.todo,details : userInput.details}},{returnOriginal : false},(err,result)=>{
+	db.getDB().collection(collection).findOneAndUpdate({_id : db.getPrimaryKey(todoID)},{$set : {name : userInput.name,global_settings : userInput.global_settings, race_settings : userInput.race_settings, rider_settings : userInput.rider_settings}},{returnOriginal : false},(err,result)=>{
 		if(err){
 			console.log("error when updating err = " + err);
 		}
 		else{
 			res.json(result);
 		}
-
 	});
 });
 
 app.post("/",(req,res,next) => {
 	const userInput = req.body;
-
+	console.log("save experiment " + JSON.stringify(userInput));
 	Joi.validate(userInput, schema, (err,result) =>{
 		if(err){
-			const error = new Error("Invalid Input adding todo");
+			const error = new Error("Invalid Input adding experiment");
 			error.status = 400;
 			next(error);
 		}
 		else{
 			db.getDB().collection(collection).insertOne(userInput,(err,result)=>{
 				if(err){
-					const error = new Error("Failed to insert todo");
+					const error = new Error("Failed to insert experiment");
 					error.status = 400;
 					next(error);
 				}
 				else{
-					res.json({result:result.result, document: result.ops[0],msg:"Successfully inserted todo",err:null});
+					res.json({result:result.result, document: result.ops[0],msg:"Successfully inserted experiment",err:null});
 				}
 			});
 		}
@@ -99,8 +102,8 @@ db.connect((err)=>{
 		process.exit(1); //terminate app
 	}
 	else{
-		app.listen(3000,()=>{
-			console.log('connected to database... listening on port 3000');
+		app.listen(3003,()=>{
+			console.log('connected to database... listening on port 3003');
 		});
 	}
 });
